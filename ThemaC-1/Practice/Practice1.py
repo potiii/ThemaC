ba415bb4124756fa53e62d227f43ccba067abb5d
potii <potiii.17@gmail.com>
2020-05-22 11:49:23 +0900

    aaaa

---
+++ ThemaC-1/Practice/Practice1.py
@@ -0,0 +1,11 @@
+price = int(input('金額(円)>'))
+print('金額:', price, '円', sep='')
+
+money_name = {10000: '一万円札=', 5000: '五千円札=', 1000: '千円札　=',
+              500: '五百円玉=', 100: '百円玉　=', 50: '五十円玉=',
+              10: '十円玉　=', 5: '五円玉　=', 1: '一円玉　='}
+
+for item in money_name:
+    temp = price // item
+    price = price % item
+    print(money_name[item], temp, '枚')
