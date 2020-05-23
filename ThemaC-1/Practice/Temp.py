9087ed5352ee22a9ce55e87db414a3a912df0a77
potii <potiii.17@gmail.com>
2020-05-23 14:22:23 +0900

    fuck

--- ThemaC-1/Practice/Temp.py
+++ ThemaC-1/Practice/Temp.py
@@ -1,26 +1,12 @@
-# %%
-import numpy as np
-import matplotlib.pyplot as plt
-import pandas as pd
+
 import psutil
 import time
-import json
+list = []
+for i in range(20):
+    list.append(round((psutil.virtual_memory().used / (1024.0 ** 3)), 2))
+    time.sleep(0)
 
-class Pcused:
-    filename = 'memory_cpu_used.json'
+list2 = [round((psutil.virtual_memory().used / (1024.0 ** 3)), 2) or time.sleep(1) for i in range(20)]
 
-    def data_write(self):
-        data = {}
-        keyA = 'memory'
-        keyB = 'cpu'
-        for i in range(18):
-            data.setdefault(i, {})
-            data[i].setdefault(keyA, round((psutil.virtual_memory().used / (1024.0 ** 3)), 2))
-            data[i].setdefault(keyB, psutil.cpu_percent(interval=1))
-            time.sleep(0)
-        with open(self.filename, 'w', encoding='utf-8') as f:
-            json.dump(data, f, ensure_ascii=False)
-
-    def data_read(self):
-        with open(self.filename, 'r', encoding='utf-8') as f:
-            return pd.read_json(self.filename)
+print(list)
+print(list2)
--- ThemaC-1/Practice/Temp2.py
+++ ThemaC-1/Practice/Temp2.py
@@ -1,1 +1,77 @@
 # %%
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import psutil
+import time
+import json
+
+
+class Pcstats:
+    filename = 'memory_cpu_used.json'
+    keyA = 'memory'
+    keyB = 'cpu'
+
+    def stats_write(self):
+        data = {}
+        for i in range(18):
+            data.setdefault(i, {})
+            data[i].setdefault(self.keyA, round((psutil.virtual_memory().used / (1024.0 ** 3)), 2))
+            data[i].setdefault(self.keyB, psutil.cpu_percent(interval=1))
+            time.sleep(1)
+        with open(self.filename, 'w', encoding='utf-8') as f:
+            json.dump(data, f, ensure_ascii=False)
+
+    def stats_read(self):
+        return pd.read_json(self.filename)
+
+    def dict_to_mem_list(self):
+        return [self.stats_read()[i][self.keyA] for i in range(18)]
+
+        # raw = self.stats_read()
+        # mem_list = []
+
+        # for i in range(18):
+        #    mem_list.append(raw[i][self.keyA])
+
+        # return mem_list
+
+    def dict_to_cpu_list(self):
+        return [self.stats_read()[i][self.keyB] for i in range(18)]
+
+        # raw = self.stats_read()
+        # cpu_list = []
+
+        # for i in range(18):
+        #     cpu_list.append(raw[i][self.keyB])
+
+        # return cpu_list
+
+    def graph_plot(self):
+        mem_list = self.dict_to_mem_list()
+        cpu_list = self.dict_to_cpu_list()
+
+        x = np.arange(0, 180, 10)
+
+        fig = plt.figure()
+        ax1 = fig.add_subplot()
+        ax1.plot(x, mem_list, color='red', marker='o')
+        ax2 = ax1.twinx()
+        ax2.plot(x, cpu_list, color='blue', marker='o')
+        plt.xticks(np.arange(0, 180, 10))
+        plt.title('My PC Used.')
+        plt.xlabel('seconds')
+        ax1.set_ylabel('Memory/GB')
+        ax2.set_ylabel('CPU/Percent')
+        plt.grid(True)
+        plt.show()
+
+
+def main():
+    pcstats = Pcstats()
+    pcstats.stats_write()
+    pcstats.graph_plot()
+
+
+if __name__ == '__main__':
+    main()
